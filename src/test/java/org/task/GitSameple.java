9e6f4ab22891ed75534aacb46a9f01f6f7a9986c
Vijay-kick <kvijay.kick@gmail.com>
2022-01-29 11:02:11 +0530

    Kick Update Code

--- src/test/java/org/task/GitSameple.java
+++ src/test/java/org/task/GitSameple.java
@@ -5,7 +5,12 @@ private static void master() {
 	System.out.println("Master");
 
 }
+private static void kick() {
+	System.out.println("Kick");
+
+}
 public static void main(String[] args) {
 	master();
+	kick();
 }
 }
