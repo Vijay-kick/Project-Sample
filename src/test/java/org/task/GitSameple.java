2aa7073d52ad01ad00e75d96fb2610d9dddcff5e
Vijay-kick <kvijay.kick@gmail.com>
2022-01-29 11:17:45 +0530

    New Code

--- src/test/java/org/task/GitSameple.java
+++ src/test/java/org/task/GitSameple.java
@@ -5,6 +5,13 @@ private static void master() {
 	System.out.println("Master");
 
 }
+private static void new1() {
+	int a=50,b=50,sum;
+	sum=a+b;
+	System.out.println(sum);
+	System.out.println("new");	
+
+}
 private static void kick() {
 	int a=5,b=5,sum;
 	sum=a+b;
@@ -14,6 +21,7 @@ private static void kick() {
 }
 public static void main(String[] args) {
 	master();
+	new1();
 	kick();
 }
 }
